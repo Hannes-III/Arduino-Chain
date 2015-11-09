21f4184b5ff3b25e05f37f1179f64dd43ac6e065
Hannes Morgenstern <github@usw.at>
2015-11-09 13:14:38 +0100

    Lizenz und library.properties angelegt

--- src/Chain.h
+++ src/Chain.h
@@ -1,11 +1,22 @@
 /*
-DipSwitch
-Copyright (C) 2015  Hannes Morgenstern http://www.usw.at
-This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
-of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License at http://www.gnu.org/licenses .
+Chain library for Arduino
+based on Code by Anatoli Arkhipenko
+Copyright (c) Hannes Morgenstern (http://www.usw.at), All rights reserved.
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3.0 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library.
 */
+
 #if ARDUINO >= 100
   #include "Arduino.h"
 #else
